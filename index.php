b75a6814d08baca30cce596790b7f2bf87f4eb2c
Romit Basak <basak.romit@gmail.com>
2023-04-06 13:14:55 +0530

    First commit with php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php $title = "Heka"; ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1"
+	>
+	<title>Heka</title>
+	<link
+		rel="icon"
+		type="image/x-icon"
+		href="Images/logo-aru-green.png"
+	>
+	<link
+		rel="stylesheet"
+		type="text/css"
+		href="CSS/CSSMain.css"
+	>
+	<script
+		src="https://kit.fontawesome.com/c47bfd481a.js"
+		crossorigin="anonymous"
+	></script>
+	<noscript>
+		<link
+			rel="stylesheet"
+			type="text/css"
+			href="CSS/CSSNoScript.css"
+		>
+	</noscript>
+</head>
+
+<body>
+
+<?php
+	$activeNavTabs = array("Home");
+	include 'template.php';
+?>
+
+</body>
+
+</html>
